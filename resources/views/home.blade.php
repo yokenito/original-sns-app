8082f04aa873931f9f5c0d10c26f63d3172bfbc2
伊藤洋健 <iy1tasmrktk@gmail.com>
2023-06-22 01:02:52 +0900

    commit_23621

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,22 +2,10 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
-        </div>
+    <div>
+        <a href="#">知識投稿</a>
+        <a href="#">質問投稿</a>
+        <a href="#">おしゃべり場</a>
     </div>
 </div>
 @endsection
