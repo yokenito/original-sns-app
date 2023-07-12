edb71a1dd17e2cf29c86ffe898fd432650be87f3
伊藤洋健 <iy1tasmrktk@gmail.com>
2023-07-12 22:21:26 +0900

    commit_230712

---
+++ database/seeders/StampTableSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class StampTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $stamp_names = [
+            '面白い', '感動'
+        ];
+
+        $count = 1;
+
+        foreach($stamp_names as $stamp_name){
+            DB::table('stamps')->insert([
+                'stamp_status' => $count,
+                'stamp_content' => $stamp_name,
+            ]);
+            $count++;
+        }
+    }
+}
