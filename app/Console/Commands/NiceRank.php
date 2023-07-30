56ecf39b1bc40797b653f5e8bdc60488a5fda73b
伊藤洋健 <iy1tasmrktk@gmail.com>
2023-07-30 20:06:47 +0900

    commit_230730

--- app/Console/Commands/NiceRank.php
+++ app/Console/Commands/NiceRank.php
@@ -9,6 +9,7 @@ use App\Models\MonthRank;
 use App\Models\MonthRank_User;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Database\Query\JoinClause;
 
 class NiceRank extends Command
 {
@@ -37,33 +38,60 @@ class NiceRank extends Command
         $today = new Carbon('today'); 
         $today_use = substr($today,0,10);
         $monthbegin = substr($today,0,8) . "01";
+        // return Carbon::now()->startOfMonth()->toDateString();
+        
         
         // いいね数比較
-        $users = User::all();
-        foreach($users as $user){
-            $nicecount = 0;
-            $posts = $user->posts()->get();
-            foreach($posts as $post){
-                $nicecount = $nicecount + count(Nice::where('post_id',$post->id)->whereBetween('created_at', [$monthbegin, $today_use])->get());
-            }
-            // DBの時点でselect sumを行なってその上からlimit5をすれば取れる
-            // 配列に追加していけばいいじゃん　ユーザーとナイスカウントの連想配列ならそのユーザーを取れる
-            Log::debug($nicecount);
-        }
+        $usernicecount = User::join('posts', function ($join){
+            $join->on('users.id','=','posts.user_id')
+                ->where('posts.user_id', '=',1);
+        })->count();
+                        
+        Log::debug($usernicecount);
+
+
+        // $users = User::all();
+        // $usernicecount=[];
+        // foreach($users as $user){
+        //     $nicecount = 0;
+        //     $posts = $user->posts()->get();
+        //     foreach($posts as $post){
+        //         $nicecount = $nicecount + count(Nice::where('post_id',$post->id)->whereBetween('created_at', [$monthbegin, $today_use])->get());
+        //         // $nicecount = Nice::where('post_id',$post->id)->whereBetween('created_at', [$monthbegin, $today_use])->sum();
+        //         // post idを配列でとってきてwhere(post_id,[])みたいな取り方？
+        //         // user -> post ユーザーとポストのテーブルをジョインする　その後にNiceテーブルからカウントさむ
+        //         // ジョインジョインでカウントサム（２、３個ジョインする必要がある）
+        //     }
+        //     // DBの時点でselect sumを行なってその上からlimit5をすれば取れる
+        //     $usernicecount[$user->id] = $nicecount;
+        //     // $usernicecount[] = array[ "id" => $user->id, "count" => $nicecount];
+        //     // $usercount[$i]["id"]
+        //     Log::debug($nicecount);
+        // }
+        // arsort($usernicecount);
+        // Log::debug($usernicecount);
+        // array_splice($usernicecount,5,count($usernicecount));
+        // Log::debug($usernicecount);
 
         // 称号
-        for($i=1; $i<=5; $i++){
-            // $monthrank = new MonthRank();
-            // $monthrank->monthrank_no = 91 . $today->year . $today->month . 0 . $i;
-            // $monthrank->monthrank_name = $today->year . "年" . $today->month . "月" . "いいね数" . $i . "位";
-            // $monthrank->monthrank_count = 0;
-            // $monthrank->save();
+        // $i = 1;
+        // foreach($usernicecount as $key =>$value){
+        //     $monthrank = new MonthRank();
+        //     $monthrank->monthrank_no = 91 . $today->year . $today->month . 0 . $i;
+        //     $monthrank->monthrank_name = $today->year . "年" . $today->month . "月" . "いいね数" . $i . "位";
+        //     $monthrank->monthrank_count = $value;
+        //     $monthrank->save();
 
-            // $monthrank_user = new User_Usertitle();
-            // $monthrank_user->user_id = 0; 
-            // $monthrank_user->monthrank_id = $monthrank->id;
-            // $monthrank_user->save();
-        }
+        //     $monthrank_user = new MonthRank_User();
+        //     $monthrank_user->user_id = $key; 
+        //     $monthrank_user->monthrank_id = $monthrank->id;
+        //     $monthrank_user->save();
+        //     if($i == 5){
+        //         break;
+        //     }else{
+        //         $i++;
+        //     }
+        // }
 
 
     }
